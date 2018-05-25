0a1574a0d05a85c4f8b205337aec9303a0b2c7b3
noeRls <noe.rivals@epitech.eu>
2018-05-25 18:49:25 +0200

    work on canva script

--- Assets/scripts/CarCanva.cs
+++ Assets/scripts/CarCanva.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class CarCanva : MonoBehaviour {
 
-    
+    public Sprite[] ImageSprite;
+    public GameObject[] ImageObject;
+    private uint displayedNb = 0;
+    public const uint maxDisplay = 3;
+    private MovingType[] displayedType = new MovingType[3];
+
 	// Use this for initialization
 	void Start () {
-		
+	    	
 	}
 
     void addObject(MovingType t)
     {
-        switch (t)
-        {
+        if (displayedNb  + 1 > maxDisplay)
+            return;
+        displayedNb++;
+        ImageObject[displayedNb - 1].SetActive(true);
+        displayedType[displayedNb - 1] = t;
+    }
 
-        }
+    void removeObject(MovingType t)
+    {
+        if (displayedNb + 1 > maxDisplay)
+            return;
+        displayedNb++;
+        ImageObject[displayedNb - 1].SetActive(true);
+        displayedNb--;
     }
 
     // Update is called once per frame
