33f1ae935477fc8bb1621752a4898ef7e09c6f96
noeRls <noe.rivals@epitech.eu>
2018-05-25 18:17:49 +0200

    add taxi prephab and start to work on canva

---
+++ Assets/scripts/CarCanva.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCanva : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void addObject(MovingType t)
+    {
+        switch (t)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+	void Update () {
+		
+	}
+}
