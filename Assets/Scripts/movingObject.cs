01c15e4d32a8859668df40c5e20609f19806f549
Yooooomi <timothee.boussus@gmail.com>
2018-05-25 18:17:11 +0200

    adding car

---
+++ Assets/Scripts/movingObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MovingType
+{
+    PERSON,
+    TRAFFIC_LIGHT,
+    OBJECT,
+}
+
+public class movingObject : MonoBehaviour {
+
+    public MovingType type;
+
+
+}
