4076f4911210bf35d6bb46d86cfe971a0a19b30f
Yooooomi <timothee.boussus@gmail.com>
2018-05-25 23:03:59 +0200

    working on transitions

---
+++ Assets/Scripts/smartCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smartCam : MonoBehaviour {
+
+    public GameObject toFollow;
+    public float height;
+
+    private void Update()
+    {
+        if (toFollow)
+        {
+            transform.rotation = toFollow.transform.rotation;
+            if (Vector3.Distance(transform.position, toFollow.transform.position) < 1)
+            {
+                transform.position = Vector3.Lerp(transform.position, toFollow.transform.position + new Vector3(0, height, 0), 10.0f * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, toFollow.transform.position + new Vector3(0, height, 0), 5.0f * Time.deltaTime);
+            }
+        }
+    }
+
+}
