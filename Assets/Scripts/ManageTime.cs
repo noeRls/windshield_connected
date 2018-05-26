46558c7c20c855f39eba67ed9d4bcf194af0e3a3
Yooooomi <timothee.boussus@gmail.com>
2018-05-26 11:39:53 +0200

    added pause

---
+++ Assets/Scripts/ManageTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageTime : MonoBehaviour {
+
+    private bool paused = false;
+
+    private void Update()
+    {
+        bool pause = Input.GetKeyDown(KeyCode.Space);
+
+        if (!paused && pause)
+        {
+            paused = true;
+            Time.timeScale = 0;
+        }
+        else if (paused && pause)
+        {
+            paused = false;
+            Time.timeScale = 1;
+        }
+    }
+
+}
