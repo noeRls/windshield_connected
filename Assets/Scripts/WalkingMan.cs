76d8b7dc495a2fc97fc85e016c2c8a75050c88fc
Yooooomi <timothee.boussus@gmail.com>
2018-05-26 11:47:51 +0200

    guys disappear when being away from car

--- Assets/Scripts/WalkingMan.cs
+++ Assets/Scripts/WalkingMan.cs
@@ -8,9 +8,11 @@ public class WalkingMan : MonoBehaviour {
     private bool began;
     private float distToGo;
     private float walkingSpeed;
+    private MeshRenderer render;
 
     private void Start()
     {
+        render = GetComponentInChildren<MeshRenderer>();
         taxi = GameObject.Find("Taxi");
         distToGo = Random.Range(15, 35);
         walkingSpeed = Random.Range(2, 8);
@@ -18,7 +20,10 @@ public class WalkingMan : MonoBehaviour {
 
     private void Update()
     {
-        if (!began && Vector3.Distance(transform.position, taxi.transform.position) < distToGo)
+        float dist = Vector3.Distance(transform.position, taxi.transform.position);
+
+        render.enabled = dist < 75;
+        if (!began && dist < distToGo)
         {
             began = true;
         }
